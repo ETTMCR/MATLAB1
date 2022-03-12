136903cd5642e6ace25134ab2eb5d4da7b9ca7f0
Eliran Tal <96107424+ETTMCR@users.noreply.github.com>
2022-03-13 01:11:15 +0200

    Update et_plot_all_xline_events.m
    additional condition if the legend is empty, than handle as needed.

--- et_plot_all_xline_events.m
+++ et_plot_all_xline_events.m
@@ -21,10 +21,17 @@ end
 figure(num_of_fig) % by using the previous if nargin==0, you actually don't need this line
 
 %hold on
+
 %% saving ORG legend
 %Legend_ORG_length= findobj(gcf, 'Type', 'Legend'); %findobj(1,'type','line') % Finds only lines in figure 1.
 Legend_ORG_length= findobj(num_of_fig, 'Type', 'Legend');
-ORG_lgnd_size=size(Legend_ORG_length.String);
+%if not(size(Legend_ORG_lengt)==0) % i.e. there's no legend  %et_plot_pupil_asym([]);
+%if not(isNull(Legend_ORG_length)) % i.e. there's no legend %et_plot_pupil_asym([]);
+if not(isempty(Legend_ORG_length)) % i.e. there's no legend %et_plot_pupil_asym([]);
+    ORG_lgnd_size=size(Legend_ORG_length.String);
+else % i.e. isempty
+    does_isempty=true;
+end
 
 %% drawing the xlines by events time
 % in order to use num_of_figure, I need to make thses cell specific to the fig
@@ -38,23 +45,24 @@ end
 hold off
 
 %% copy legend elements 
+if not(does_isempty) %i.e. there is legend
+    % https://www.mathworks.com/matlabcentral/answers/163444-matlab-2014b-copying-just-the-legend-from-existing-figure-into-subplot
+    %hLegend = findobj(gcf, 'Type', 'Legend');
+    hLegend = findobj(num_of_fig, 'Type', 'Legend');
 
-% https://www.mathworks.com/matlabcentral/answers/163444-matlab-2014b-copying-just-the-legend-from-existing-figure-into-subplot
-%hLegend = findobj(gcf, 'Type', 'Legend');
-hLegend = findobj(num_of_fig, 'Type', 'Legend');
+    %https://www.mathworks.com/matlabcentral/answers/62393-adding-legend-in-a-plot-genereted-by-a-loop
+    Legends=hLegend.String,1;
+     ORG_Legends=[]; %preventing from old legends to be written
+    for i=1:ORG_lgnd_size(2)
+        ORG_Legends{i}=string(Legends(i)) ;
+    end
+    legend(ORG_Legends);
+    %legend(hLegend.String{2},hLegend.String{1}) %positioning string legends in specific places
 
-%https://www.mathworks.com/matlabcentral/answers/62393-adding-legend-in-a-plot-genereted-by-a-loop
-Legends=hLegend.String,1;
- ORG_Legends=[]; %preventing from old legends to be written
-for i=1:ORG_lgnd_size(2)
-    ORG_Legends{i}=string(Legends(i)) ;
-end
-legend(ORG_Legends);
-%legend(hLegend.String{2},hLegend.String{1}) %positioning string legends in specific places
+    %shg % -last Handle to be shown 
+    %set(gcf, 'Position', get(0, 'Screensize')); % making a full screen
+    set(num_of_fig, 'Position', get(0, 'Screensize')); % making a full screen
+    figure(num_of_fig) %the figure to focus
+end % if not(does_isempty) %i.e. there is legend
 
-%shg % -last Handle to be shown 
-%set(gcf, 'Position', get(0, 'Screensize')); % making a full screen
-set(num_of_fig, 'Position', get(0, 'Screensize')); % making a full screen
-figure(num_of_fig) %the figure to focus
-
-end
+end %of function
